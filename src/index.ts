e35094588b4e33211d73ce146d6c698cdc2805d7
Ezimoha Chigemezu Hipolitus <chigemezuezimoha@gmail.com>
2026-03-28 11:28:35 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = 8000;
+
+// JSON middleware
+app.use(express.json());
+
+// Root route
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Hello from TypeScript Express server!" });
+});
+
+app.listen(PORT, () => {
+  const url = `http://localhost:${PORT}`;
+  console.log(`Server started and listening on ${url}`);
+});
